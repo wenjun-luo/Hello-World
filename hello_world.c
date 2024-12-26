2062e643ad9225d7cad30663b3e48353240d2db8
lwj <T@example.com>
2024-12-26 15:44:41 +0800

    B had change hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -5,7 +5,11 @@ int main()
 
     printf("Hello World!\n");
     printf("I am a student\n");
-     printf("I am a puple\n");
-      printf("I am a teacher\n");
+    printf("I am a puple\n");
+    printf("I am a teacher\n");
+    printf("I am a teacher\n");
+    printf("I am a teacher\n");
+
+    printf("I am a teacher\n");
     return 0;
 }
